2ab37537394211c3448b28311c90df8198493a64
Jon Duell <jon@jonduell.com>
2014-12-09 13:48:30 -0800

    Fixes display of select main menu

--- template.php
+++ template.php
@@ -289,3 +289,14 @@ function newspaper_theme_page_alter(&$page) {
 
   drupal_add_html_head($meta_viewport, 'meta_viewport');
 }
+
+/**
+ * Implements hook_block_view_alter().
+ */
+function newspaper_theme_block_view_alter(&$data, $block) {
+  // Remove visible-xs class from select menu, since it interferes with select2.
+  if (in_array($block->module, array('menu_block'))) {
+    $classes =& $data['content']['#content']['select']['#attributes']['class'];
+    unset($classes[array_search('visible-xs', $classes)]);
+  }
+}
